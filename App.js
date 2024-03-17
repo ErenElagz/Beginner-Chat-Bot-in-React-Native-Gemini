9449455e1516846e3bf6f696bd4055a0996270f0
Eren <erenelagoz@gmail.com>
2024-03-17 13:34:26 +0300

    rg

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useRef } from "react";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity, TextInput } from "react-native";
 
