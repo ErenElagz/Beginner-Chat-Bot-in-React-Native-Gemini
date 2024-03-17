e9b23006ed221a5c87da0ee563b535167b42f37c
Eren <erenelagoz@gmail.com>
2024-03-17 13:31:36 +0300

    werwe

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
-import react, { useState } from "react";
+import React, { useCallback, useMemo, useRef } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, SectionList, Image, SafeAreaView, ScrollView, TextInput } from "react-native";
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity, TextInput } from "react-native";
 
 export default function App() {
 	return (
@@ -10,75 +9,71 @@ export default function App() {
 
 			{/* Header */}
 			<View style={styles.header}>
-				<Image source={require("./assets/icons/back-arrow.png")} style={styles.icon} />
-				<View style={styles.content}>
+				<TouchableOpacity>
+					<Image source={require("./assets/icons/left-arrow.png")} style={styles.icon} />
+				</TouchableOpacity>
+				<View style={{ flexDirection: "row", alignItems: "center", gap: 4 }}>
 					<Image source={require("./assets/logo.png")} style={styles.icon} />
-					<Text style={styles.title}>AtlasAI</Text>
+					<Text style={{ fontSize: 24, fontWeight: "700", color: "#323232" }}>AtlasAI</Text>
 				</View>
-				<Image source={require("./assets/icons/menu.png")} style={styles.icon} />
+				<TouchableOpacity>
+					<Image source={require("./assets/icons/menu.png")} style={styles.icon} />
+				</TouchableOpacity>
 			</View>
+
 			{/* Content */}
-			<ScrollView></ScrollView>
+			<ScrollView>
+				<Text></Text>
+			</ScrollView>
+
 			{/* Search-Bar */}
-			<View style={styles.search}>
+			<View style={styles.searchBar}>
 				<TextInput placeholder="Ask to Atlas AI" style={styles.input}></TextInput>
-				<Image source={require("./assets/icons/arrow-right.png")} style={{ width: 32, height: 32, opacity: 0.5 }} />
+				<TouchableOpacity>
+					<Image source={require("./assets/icons/right-arrow.png")} style={styles.icon} />
+				</TouchableOpacity>
 			</View>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
-	prompts: {
-		alignItems: "left",
-		paddingHorizontal: 20,
-		marginTop: 16,
+	container: {
+		backgroundColor: "#ffffff",
+		flex: 1,
+		paddingTop: 16,
 	},
 	header: {
 		width: "100%",
 		flexDirection: "row",
 		justifyContent: "space-between",
 		alignItems: "center",
-		paddingHorizontal: 20,
-	},
-	content: {
-		flexDirection: "row",
-		justifyContent: "center",
-		alignItems: "center",
-		gap: 4,
-	},
-	title: {
-		fontSize: 24,
-		fontWeight: "700",
-		color: "#323232",
-	},
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		marginTop: 48,
+		paddingHorizontal: 16,
+		paddingTop: 24,
+		paddingBottom: 16,
 	},
 	icon: {
 		width: 24,
 		height: 24,
 	},
-	input: {
-		width: "92%",
-		borderRadius: 32,
-		paddingVertical: 12,
-		paddingHorizontal: 20,
-		backgroundColor: "#f1f2f3",
-		fontSize: 16,
-	},
-	search: {
+	searchBar: {
+		backgroundColor: "#ffffff",
+		width: "100%",
 		position: "absolute",
 		bottom: 0,
 		flexDirection: "row",
 		justifyContent: "center",
 		alignItems: "center",
-		paddingHorizontal: 20,
-		gap: 4,
+		paddingHorizontal: 32,
 		paddingVertical: 12,
+		gap: 8,
+	},
+	input: {
 		backgroundColor: "#fafafa",
+		width: "100%",
+		fontSize: 16,
+		paddingVertical: 16,
+		paddingHorizontal: 24,
+		borderRadius: 32,
 	},
 });
